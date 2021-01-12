30977738af15a2a4bf5e0ca49bcbdb490e50263e
RAH-ORBIT <75024639+RAH-ORBIT@users.noreply.github.com>
2021-01-12 11:17:45 +0100

    Update AtCtrl.ts

--- AtCtrl.ts
+++ AtCtrl.ts
@@ -35,12 +35,16 @@ namespace atcontrol {
         let recevice_text: string = "";
         const timeout: number = 10000;
         let time_at_depature: number = 0;
+
+        led.plot(0,0);
+
         while(true)
         {
             if (current_cmd === undefined) {
                 current_cmd = cmd_queue.pop();
                 if (current_cmd !== undefined)
                 {
+                    led.plot(1,0);
                     serial.writeString(current_cmd.cmd + at_line_delimiter);
                     time_at_depature = input.runningTime();
                 }
@@ -50,17 +54,20 @@ namespace atcontrol {
             let lines = recevice_text.split(at_line_delimiter);
             let line: string | undefined = undefined;
             if (lines.length > 1) {
+                led.plot(2,0);
                 line = lines[0]; 
                 recevice_text = lines[1];
             }
                 
             if (current_cmd !== undefined) {
                 if ((line !== undefined && line.includes(current_cmd.ok_match))) {
+                    led.plot(3,0);
                     current_cmd.onCmp();
                     current_cmd = undefined;
                 }
                 if ((line !== undefined && line.includes(current_cmd.error_match)) ||
                     (input.runningTime() - time_at_depature) > timeout) {
+                    led.plot(4,0);
                     current_cmd.onError();
                     current_cmd = undefined;
                 }

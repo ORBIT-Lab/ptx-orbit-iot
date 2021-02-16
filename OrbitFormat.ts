8fa7b24da99615620a75a7d7ab0f707373de143a
RuneHeick <hteml@hotmail.com>
2021-02-16 11:57:17 +0100

    fixed json bug

--- OrbitFormat.ts
+++ OrbitFormat.ts
@@ -8,9 +8,9 @@ namespace Orbit_Format
         let packet = "{"
         packet += "\"uid\":" + serial + ","
         packet += "\"cmd\":\""+cmd+"\","
-        packet += "\"payload\":\""+value+"\""
+        packet += "\"payload\":\""+value+"\","
         if(institution !== "")
-            packet += "\"institution_uid\":\""+institution+"\","
+            packet += "\"institution_uid\":\""+institution+"\""
         packet += "}"
         return packet;
     }

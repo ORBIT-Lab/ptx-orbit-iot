43d266e63c4aec904227295d7388e5e4cbc04539
RuneHeick <hteml@hotmail.com>
2022-08-15 12:40:43 +0200

    crypto started

---
+++ Crypto.ts
@@ -0,0 +1,14 @@
+// Tilføj din kode her
+namespace Orbit_IoT {
+
+    //% block="Encrypt" weight=5
+    //% block.loc.da="Krypter"
+    //% subcategory="Encrypt"
+    export function encrypt(name: string) {
+        return name
+    }
+
+
+
+
+}

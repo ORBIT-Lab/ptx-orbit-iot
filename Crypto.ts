0078cc66c67f84bfb8b758ecb38dd620811cd37b
RuneHeick <hteml@hotmail.com>
2022-08-16 10:57:12 +0200

    fixed crypto and removed æøå

--- Crypto.ts
+++ Crypto.ts
@@ -1,7 +1,7 @@
 // Tilføj din kode her
 namespace Orbit_IoT {
 
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
     //% block="Encrypt %data with cesar code %skip" weight=4
@@ -12,7 +12,10 @@ namespace Orbit_IoT {
         for (let i = 0; i < data.length; i++) 
         {
             let pos = alphabet.indexOf(data[i]);
-            ret += alphabet[(pos + skip) % alphabet.length]
+            if(pos == -1)
+                ret += data[i]
+            else
+                ret += alphabet[(pos + skip) % alphabet.length]
         } 
         return ret
     }
@@ -24,10 +27,15 @@ namespace Orbit_IoT {
         let ret: string = ''
         for (let i = 0; i < data.length; i++) {
             let pos = alphabet.indexOf(data[i]);
-            let decrypted_pos = (pos - skip)
-            while(decrypted_pos < 0)
-                decrypted_pos = alphabet.length + decrypted_pos
-            ret += alphabet[decrypted_pos]
+            if(pos == -1)
+                ret += data[i]
+            else
+            {
+                let decrypted_pos = (pos - skip)
+                while(decrypted_pos < 0)
+                    decrypted_pos = alphabet.length + decrypted_pos
+                ret += alphabet[decrypted_pos]
+            }
         }
         return ret
     }
